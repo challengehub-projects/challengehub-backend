6af12df5204a6b44fba3a0713f8fdf286bdaa4ff
challengehub-projects <greatajoku36@gmail.com>
2026-05-20 10:34:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use("/api/", examRoutes);
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/", () => {
+  console.log("Running...");
+})
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
